64a981cb96923498cfd69dee1aed2e5297b7ea0f
Federico Maiello <federicomaiello@hotmail.it>
2023-10-26 20:56:34 +0200

    esercizio 20

---
+++ src/Esercizio20.java
@@ -0,0 +1,15 @@
+public class Esercizio20 {
+    public static void main(String[] args) {
+        int x = 0;
+        risultato(x);
+    }
+
+    public static int risultato(int x) {
+        for (x=0;x<=10;x++){
+            int y = 4;
+            System.out.println(x*y);
+        }
+        return x;
+    }
+
+}
