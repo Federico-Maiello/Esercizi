331c781091ed5c2d68e9b358981436e4035c06d2
Federico Maiello <federicomaiello@hotmail.it>
2023-10-28 19:09:20 +0200

    esercizio 27

---
+++ src/Esercizio27.java
@@ -0,0 +1,31 @@
+public class Esercizio27 {
+    public static void main(String[] args) {
+        int[][] matrix = goMatrix();
+        printMatrix(matrix);
+
+        int sumMatrix = sumMatrix(matrix);
+        System.out.println("La somma della prima riga è: " + sumMatrix);
+    }
+
+    public static int[][] goMatrix() {
+        int[][] matrix= {{1, 2, 4,12},{8, 9, 5 ,55}};
+        return matrix;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i=0; i< matrix.length; i++){
+            for (int j=0; j< matrix[i].length; j++){
+                System.out.println(matrix[i][j] );
+            }
+            System.out.println();
+        }
+    }
+
+    public static int sumMatrix(int[][] matrix) {
+        int sum=0;
+        for (int j=0; j<matrix[0].length; j++){
+            sum+= matrix[0][j];
+        }
+        return sum;
+    }
+}
