07a719716522f96eca744b6149f4f0d4d9ff609a
Federico Maiello <federicomaiello@hotmail.it>
2023-11-02 15:05:55 +0100

    Esercizio 29 corretto

--- src/Esercizio29/Esercizio29.java
+++ src/Esercizio29/Esercizio29.java
@@ -1,8 +1,18 @@
 package Esercizio29;
 
-public class Esercizio29 {
+//public class Esercizio29 {
+//    public static void main(String[] args) {
+//        Studente student1 = new Studente ("Federico", "Maiello", 225);
+//        System.out.println("Studente: " + student1);
+//    }
+//}
+
+public class Esercizio29{
     public static void main(String[] args) {
-        Studente student1 = new Studente ("Federico", "Maiello", 225);
+        Studente student1 = new Studente("Federico", "Maiello", 544);
         System.out.println("Studente: " + student1);
     }
 }
+
+
+
