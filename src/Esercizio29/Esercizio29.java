22bd65243243117451a8e0de3952d51f253edf47
Federico Maiello <federicomaiello@hotmail.it>
2023-11-03 13:55:13 +0100

    esercizio 30

--- src/Esercizio29/Esercizio29.java
+++ src/Esercizio29/Esercizio29.java
@@ -1,11 +1,5 @@
 package Esercizio29;
 
-//public class Esercizio29 {
-//    public static void main(String[] args) {
-//        Studente student1 = new Studente ("Federico", "Maiello", 225);
-//        System.out.println("Studente: " + student1);
-//    }
-//}
 
 public class Esercizio29{
     public static void main(String[] args) {
--- src/Esercizio30/Esercizio30.java
+++ src/Esercizio30/Esercizio30.java
@@ -1,8 +1,9 @@
 package Esercizio30;
 
-public class Esercizio30 {
+
+public class Esercizio30{
     public static void main(String[] args) {
-        Auto car1 = new Auto (1800, "ED999HH", "Mitsubishi", "Lancer");
+        Auto car1 = new Auto(1800, "TT555GG", "Fiat", "500");
         System.out.println("Auto: " + car1);
     }
 }
